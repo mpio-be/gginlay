2737f56de0c3db5249735bec0119cb8b77397087
Mihai Valcu <valcu@orn.mpg.de>
2023-03-25 21:00:42 +0100

    main function done!

--- R/aaa.R
+++ R/aaa.R
@@ -1,6 +1,6 @@
 
 #' @import     methods ggplot2 shiny gridlayout  bslib
-#' @importFrom patchwork inset_element
+#' @importFrom patchwork inset_element plot_annotation
 #' @importFrom shinyWidgets noUiSliderInput switchInput
 #' @importFrom grid rectGrob gpar unit
 #'
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,32 @@
+.emptyGG <- function(xmax, ymax) {
+  x <- data.frame(
+    y = seq(0, ymax, length.out = 10),
+    x = seq(0, xmax, length.out = 10)
+  )
+
+  ggplot(x) +
+    geom_line(aes(y = y, x = x), color = "red", linewidth = 0.2) +
+    geom_line(aes(y = rev(y), x = x), color = "red", linewidth = 0.2) +
+    xlab(NULL) +
+    ylab(NULL) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    theme_linedraw() +
+    theme(
+      plot.margin = margin(t = 0, r = 0, b = 0, l = 0),
+      panel.background = element_rect(fill = "transparent"),
+      plot.background = element_rect(fill = "transparent", color = NA),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      axis.line = element_blank(),
+      axis.ticks = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank()
+    )
+
+}
+
+
 
 #' @export
 emptyGG <- function(x) UseMethod("emptyGG")
@@ -12,38 +41,24 @@ emptyGG.default <- function(x, ...) {
 
 
 #' @export
-emptyGG.nativeRaster <- function(r, keep.ratio = TRUE) { 
+emptyGG.nativeRaster <- function(r) {
+  .emptyGG(xmax = ncol(r), ymax = nrow(r)) +
+    coord_fixed()
+}
 
-  x = data.frame(
-    y = seq(0, nrow(r), length.out = 10),
-    x = seq(0, ncol(r), length.out = 10)
-  )
+#' @export
+emptyGG.raster <- function(r) {
+  emptyGG.nativeRaster(r)  
+}
 
-  g = ggplot(x) +
-    geom_line(aes(y = y, x = x), color = "red", linewidth = 0.2) +
-    geom_line(aes(y = rev(y), x = x), color = "red", linewidth = 0.2) +
-    xlab(NULL) +
-    ylab(NULL) +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_continuous(expand = c(0, 0)) +
-    theme_linedraw() +
-  
-    theme(
-      plot.margin = margin(t = 0, r = 0, b = 0, l = 0), 
-      panel.background = element_rect(fill='transparent'), 
-      plot.background = element_rect(fill='transparent', color = NA), 
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_blank(),
-      axis.line = element_blank(),
-      axis.ticks = element_blank(),
-      axis.text.x = element_blank(),
-      axis.text.y = element_blank()
-    )
+#' @export
+emptyGG.ggplot <- function(g) { 
 
-  if (keep.ratio) {
-    g = g + coord_fixed()
-  }
-  
-  g
+  xmax = ggplot_build(g)$layout$panel_params[[1]]$x.range |> diff()
+  ymax = ggplot_build(g)$layout$panel_params[[1]]$y.range |> diff()
+
+  .emptyGG(xmax = xmax, ymax = ymax)
+
+
 
 }
